bfb52b069d7a80bdf24db9717d4fe0a8cc03f315
akanksha675 <akankshachoudhari19@gmail.com>
2025-01-31 23:41:39 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,25 @@
+// var a=200
+// {
+//    var a=400;
+  
+// }
+// let b=a
+// {
+//    let b=-400
+// }
+// console.log(b)
+
+// const arr=[1,2,3,4,5]
+// console.log(arr.includes(2))
+// let str1="krishna";
+// let str2="veeni";
+// console.log(str1.concat(str2))
+
+// let arr=[300,8,900,400,6]
+// arr.sort((a,b)=>{
+//     return b-a
+// })
+// console.log(arr)
+// a=100
+a=100
+console.log(a) 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let str1="krishna";
+// let str2="veeni";
+// console.log(str1.concat(str2))
